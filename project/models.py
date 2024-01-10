caae685ffc17a6704b923b384a4e8fe37761be4e
Эдзэн Ойчиев <edzenojciev@MacBook-Air-Edzen.local>
2024-01-10 21:06:40 +0600

    какой то урок и передаю Эржану

--- project/models.py
+++ project/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from autoslug import AutoSlugField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, verbose_name="название категории")
+    slug = AutoSlugField(populate_from="name", unique=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(verbose_name='название', max_length=30)
+    img = models.ImageField(upload_to='product/', verbose_name='фотки')
     desc = models.TextField(verbose_name='описание')
     price = models.FloatField(verbose_name='цена', max_length=10)
 
     def __str__(self) -> str:
-        return self.name
+        return f"{self.name}   -  Категория:  {self.category.name}"
 
