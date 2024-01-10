341e071ab48eee91c6310a76f1fd6d0b76b1e1ff
salievvv <erzansaliev95@gmail.com>
2024-01-10 20:13:08 +0600

    moe dz

--- project/models.py
+++ project/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(verbose_name='название', max_length=30)
+    desc = models.TextField(verbose_name='описание')
+    price = models.FloatField(verbose_name='цена', max_length=10)
+
+    def __str__(self) -> str:
+        return self.name
+
