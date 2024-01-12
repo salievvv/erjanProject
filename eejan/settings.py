63cffa9a090751d0e4902c6205ad190878388f48
Эдзэн Ойчиев <edzenojciev@MacBook-Air-Edzen.local>
2024-01-12 20:40:01 +0600

    исправил баг

--- eejan/settings.py
+++ eejan/settings.py
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = ''
+STATIC_URL = '/static/'
+STATIC_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
