a49ee41af632c438b4b847da687d81664f5bf02e
salievvv <erzansaliev95@gmail.com>
2024-01-16 18:24:48 +0600

    popytki perehoda

--- project/models.py
+++ project/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from autoslug import AutoSlugField
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=20,verbose_name='название категории')
@@ -10,10 +11,18 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,null=True)
     name = models.CharField(verbose_name='название', max_length=30)
-    img = models.ImageField(upload_to='product ',verbose_name='фотки')
+    img = models.ImageField(upload_to='product/',verbose_name='фотки')
     desc = models.TextField(verbose_name='описание')
     price = models.FloatField(verbose_name='цена')
 
     def __str__(self) -> str:
         return self.name
+
+class ProAbout(models.Model):
+
+    
+    def get_absolute_url(self):
+        return reverse('about', kwargs={'about'})
+
+
 
--- project/views.py
+++ project/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Product
-
+from .models import ProAbout
 
 def index(request):
     products = Product.objects.all()
     context = {'products':products}
     return render(request,'index.html',context)
+
+def about(request):
+    return render(request,'proabout/about.html')
